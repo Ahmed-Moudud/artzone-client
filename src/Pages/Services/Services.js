1ba952542712aa87677612e7dd6adbcaaea58cdb
Ahmed-Moudud <suzan.4bd@gmail.com>
2022-12-22 00:10:45 +0600

    nothing changed

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -3,11 +3,15 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    
+    
     useEffect(() =>{
         fetch('https://assignment-eleven-server.vercel.app/services')
         .then(res => res.json())
         .then(data => setServices(data))
     }, [])
+
+   
     return (
         <div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-3'>                
